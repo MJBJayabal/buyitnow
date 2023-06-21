728debc1b00652e42d9e3c976658377e0a93197e
Jayabal <jayabal99942@gmail.com>
2023-06-21 09:48:53 +0530

    butitnow

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+
+const getProducts = async () => {
+  const { data } = await axios.get(`${process.env.API_URL}/api/products`)
+  return data
+}
+
+const HomePage = async () => {
+
+  const productsData = await getProducts()
+
+  return <ListProducts data={productsData} />
+};
+
+export default HomePage;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        appDir: true,
+      },
+      env: {
+        API_URL: "http://localhost:3000",
+        DB_URI: "mongodb://localhost:27017/buyitnow",
+      },
+      images: {
+        domains: ["res.cloudinary.com"],
+      },
+};
 
 module.exports = nextConfig
